4941d2fb0a9435a242c5f0f0920f80049716df64
Dean <me@deann.dev>
2024-05-07 16:59:19 -0700

    Break states into pitch counts vs. final states

--- pitch_count.py
+++ pitch_count.py
@@ -3,7 +3,7 @@ from fsm import MooreMachine, State
 pitch_count = MooreMachine('Pitch Count')
 balls = 0
 strikes = 0
-states = {'0-0': State('0-1', initial=True), '0-2': State('1-0'), 
+count_states = {'0-0': State('0-1', initial=True), '0-2': State('1-0'), 
           '1-1': State('1-2'), '2-0': State('2-1'),
-          '2-2': State('3-0'), '3-1': State('3-2'),
-          'Base': State('Base'), 'Out': State('Out')}
+          '2-2': State('3-0'), '3-1': State('3-2')}
+accepting_states = {'Base': State('Base'), 'Out': State('Out')}
