eb2ed600308434beb110ecbf8cfb2c81d15aaa1c
Dean <me@deann.dev>
2024-05-07 17:45:12 -0700

    Streamline state creation

--- pitch_count.py
+++ pitch_count.py
@@ -3,11 +3,7 @@ from fsm import MooreMachine, State
 pitch_count = MooreMachine('Pitch Count')
 balls = 0
 strikes = 0
-count_states = {(0, 0): State('0-0', initial=True), (0, 1): State('0-1'), (0, 2): State('0-2'), 
-                (1, 0): State('1-0'), (1, 1): State('1-1'), (1, 2): State('1-2'),
-                (2, 0): State('2-0'), (2, 1): State('2-1'),
-                (2, 2): State('2-2'), (3, 0): State('3-0'),
-                (3, 1): State('3-1'), (3, 2): State('3-2')}
+count_states = {(i, j) : State(f'{i}-{j}', initial=(i == 0 and j == 0)) for i in range(4) for j in range(3)}
 
 accepting_states = {'Base': State('Base'), 'Out': State('Out')}
 for key, state in count_states.items():
